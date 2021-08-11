360782d900f98c7b4fe5153892b53defb5a5df54
shakibaSHS <88477895+shakibaSHS@users.noreply.github.com>
2021-08-11 19:13:52 +0430

    Add files via upload

---
+++ s2h_1_Even_or_Odd.py
@@ -0,0 +1,14 @@
+number =  float(input("insert your number\n number= "))
+
+temp = number % 2
+
+if (temp == 0):
+
+    print('your number is Even')
+
+elif  (temp == 1):
+
+    print('your number is Odd')
+
+else:
+    print('your number is not integer')
---
+++ s2h_2_divisible.py
@@ -0,0 +1,12 @@
+number1 = float(input("insert your first number as an integer number\n number1= "))
+number2 = float(input("insert your second number as an integer number\n number2= "))
+
+temp = number1 % number2
+
+if (temp == 0 )  :
+
+    print('your numbers are divisible.')
+
+else:
+
+    print('your numbers are indivisible.')
