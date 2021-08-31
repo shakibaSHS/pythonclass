aa3a054be3ea72312583cffd95ee328730322987
shakibaSHS <88477895+shakibaSHS@users.noreply.github.com>
2021-08-31 19:52:54 +0430

    Add files via upload

---
+++ s4h_2_dict.py
@@ -0,0 +1,5 @@
+name = input('Please insert your name \n name=')
+age = input('Please insert your age \n age=')
+martial_status = input('married or single\n m/s?=')
+introduction = {'name':name , 'age':age , 'martial status':martial_status}
+print(f'Your personal information is = {introduction}')
