c6389525fb5efdd121b0667bfcd9471f77b609e4
shakibaSHS <88477895+shakibaSHS@users.noreply.github.com>
2021-08-31 19:52:29 +0430

    Add files via upload

---
+++ s4h_1_intersection_union_riaziphysic.py
@@ -0,0 +1,6 @@
+riazi1 = {'reza', 'ali','hamid','sara', 'negar'}
+physic1 = {'reza','hasan','amir','sara','negar'}
+
+print(f'The intersection of two class is = {riazi1.intersection(physic1)}')
+print(f'The union of two class is = {riazi1.union(physic1)}')
+print(f'The diference between two class is = {riazi1 - physic1}')
